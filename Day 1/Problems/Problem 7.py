aebf66b1044293dff8e431dd6e7cff460d705efd
Magnus Moan <magnus.moan@visma.com>
2018-10-23 22:46:32 +0200

    Updated problems and added problem 7

---
+++ Day 1/Problems/Problem 7.py
@@ -0,0 +1,13 @@
+"""
+### Problem 7: File handling, importing, dictionaries and functions
+This problem is divided into 3 parts, and each part could potentially be its own function, but that's up to you:
+1. Store the *n* first Fibonacci numbers to a file called *fibonacci.txt*. Make sure to re-use the Fibonacci function
+   implemented earlier.
+2. Read the file your wrote to in the previous step and store the number of occurences of each individual character in
+   that file to a dictionary
+3. Print the key in the dictionary from the previous step that has the highest value, i.e. print the character that
+   appeared most times in the file.
+"""
+
+# Write your code here!
+
